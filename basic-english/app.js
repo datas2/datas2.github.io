fc0b37629db0f3f91d06edeb9749a0ac04c78f1f
Augusto Machado <augustomachadoramos@gmail.com>
2026-04-15 01:19:36 -0300

    Refactor code structure for improved readability and maintainability

--- basic-english/app.js
+++ basic-english/app.js
@@ -5,7 +5,7 @@
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		navigator.serviceWorker
-			.register("/service-worker.js")
+			.register("/basic-english/service-worker.js")
 			.catch((err) => console.error("SW registration failed:", err));
 	});
 }
